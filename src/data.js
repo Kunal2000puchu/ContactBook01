efbd170b839f5fc82b58700b880b64e5cdaffde7
Kuntal Acharya <kuntal@Kuntals-MacBook-Air.local>
2021-07-15 13:25:33 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default[
+    {
+        id:1,
+        name:'vinayak vajpaye',
+        number: 9876478350,
+        image:'https://images.unsplash.com/photo-1601288496920-b6154fe3626a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFjZXN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        id:2,
+        name:'akshat mahavar',
+        number: 9876478354,
+        image:'https://images.unsplash.com/photo-1606792109963-7b34205b1333?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fHNleHklMjBnaXJsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+
+        id:3,
+        name:'pankaj singh',
+        number: 9876478353,
+        image:'https://images.unsplash.com/photo-1567892320421-1c657571ea4a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c2V4eSUyMGdpcmx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+        id:4,
+        name: 'tushar rao',
+        number: 9756345328,
+        image:'https://images.unsplash.com/photo-1512310604669-443f26c35f52?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fHNleHklMjBnaXJsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    },
+];
